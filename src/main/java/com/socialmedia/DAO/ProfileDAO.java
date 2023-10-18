bf7950016bd0467e743c766d00bb281dbcff0840
sefunmi <iv.vi.kyle@gmail.com>
2023-10-18 23:43:19 +0100

    user profile functionalities

--- src/main/java/com/socialmedia/DAO/ProfileDAO.java
+++ src/main/java/com/socialmedia/DAO/ProfileDAO.java
@@ -8,26 +8,47 @@ import jakarta.persistence.EntityManager;
 public class ProfileDAO {
     Util util = new Util();
 
-    public void checkProfile(String email, String pasword){
+    public void logIn(String email, String password){
+
+        if(!checkProfile(email)){
+            throw new IllegalArgumentException("profile does not exist");
+        }
+
+        EntityManager em = util.getEM();
+        var tx = em.getTransaction();
+        tx.begin();
+        List profiles = em.createQuery("SELECT * from m WHERE m.email = "+email+" AND m.password = "+password).getResultList();
+        em.close();
+
+        if(profiles.size()<1){
+            throw new IllegalArgumentException("incorrect email or password");
+        }
 
     }
 
     public void CreateProfile(Profile profile){
-        EntityManager em = util.getEM();
-        var tx = em.getTransaction();
-        tx.begin();
-        List<Profile> profiles = em.createQuery("").getResultList();
 
-        if(profiles.size()>0){
+        if(checkProfile(profile.getEmail())){
             throw new IllegalArgumentException("profile already exists");
         }
 
+        EntityManager em = util.getEM();
+        var tx = em.getTransaction();
+        tx.begin();
         em.persist(profile);
         tx.commit();
         em.close();
     }
 
-    public void deleteProfile(String email){
+    public void deleteProfile(String email, String password){
+
+        logIn(email,password);
+        EntityManager em = util.getEM();
+        var tx = em.getTransaction();
+        tx.begin();
+        em.createQuery("DELETE * from m WHERE m.email = "+email+" AND m.password = "+password).executeUpdate();
+        tx.commit();
+        em.close();
 
     }
 
@@ -35,7 +56,7 @@ public class ProfileDAO {
         EntityManager em = util.getEM();
         var tx = em.getTransaction();
         tx.begin();
-        List<Profile> profiles = em.createQuery("").getResultList();
+        List profiles = em.createQuery("SELECT * from m WHERE m.email = "+email).getResultList();
 
         if(profiles.size()>0){
             return true;
