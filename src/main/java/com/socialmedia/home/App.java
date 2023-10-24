22fbf4e8e08f46438a3b05c5fe5f3fd86f2474b5
sefunmi <iv.vi.kyle@gmail.com>
2023-10-24 22:56:32 +0100

    test update

--- src/main/java/com/socialmedia/home/App.java
+++ src/main/java/com/socialmedia/home/App.java
@@ -20,37 +20,43 @@ public class App {
     public static final int OPTION_VIEW_PERSONAL_PROFILE = 7;
 
     public static void main(String [] args){
-        System.out.println("------------------------------");
-        System.out.println("WELCOME TO THE APP!");
-        System.out.println("SELECT ONE OF THE OPTIONS BELOW TO PROCEED");
-        System.out.println("TO SIGNUP PLEASE SELECT: "+OPTION_SIGNUP);
-        System.out.println("TO LOGIN PLEASE SELECT: "+OPTION_LOGIN);
-
-        ProfileDAO pdao = new ProfileDAO();
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
+        String input = sc.nextLine();
+        System.out.println("again");
+        String input2 = sc.nextLine();
+        System.out.println("againnn");
+        String input3 = sc.nextLine();
 
-        System.out.println("Enter account email");
-        String email = sc.nextLine();
-        System.out.println("<----------->");
-        System.out.println("Enter account password");
-        String password = sc.nextLine();
-        System.out.println("<----------->");
-
-        switch(input){
-            case 1:
-                pdao.CreateProfile(new Profile(email,password));
-
-            case 2:
-                pdao.logIn(email,password);
-        }
-
-        System.out.println("WHAT OPERATION WOULD YOU LIKE TO DO NOW");
-        System.out.println("TO VIEW POSTS ENTER "+OPTION_VIEW_POSTS);
-        System.out.println("TO CREATE A POST ENTER "+OPTION_CREATE_POST);
-        System.out.println("TO SEARCH FOR POSTS ENTER "+OPTION_SEARCH_POST);
-        System.out.println("TO SEARCH FOR A PROFILE ENTER "+OPTION_SEARCH_PROFILE);
-        System.out.println("TO VIEW PERSONAL PROFILE ENTER "+OPTION_VIEW_PERSONAL_PROFILE);
+//        System.out.println("------------------------------");
+//        System.out.println("WELCOME TO THE APP!");
+//        System.out.println("SELECT ONE OF THE OPTIONS BELOW TO PROCEED");
+//        System.out.println("TO SIGNUP PLEASE SELECT: "+OPTION_SIGNUP);
+//        System.out.println("TO LOGIN PLEASE SELECT: "+OPTION_LOGIN);
+//
+//        ProfileDAO pdao = new ProfileDAO();
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+//        System.out.println("Enter account email");
+//        String email = sc.nextLine();
+//        System.out.println("<----------->");
+//        System.out.println("Enter account password");
+//        String password = sc.nextLine();
+//        System.out.println("<----------->");
+//
+//        switch(input){
+//            case 1:
+//                pdao.CreateProfile(new Profile(email,password));
+//
+//            case 2:
+//                pdao.logIn(email,password);
+//        }
+//
+//        System.out.println("WHAT OPERATION WOULD YOU LIKE TO DO NOW");
+//        System.out.println("TO VIEW POSTS ENTER "+OPTION_VIEW_POSTS);
+//        System.out.println("TO CREATE A POST ENTER "+OPTION_CREATE_POST);
+//        System.out.println("TO SEARCH FOR POSTS ENTER "+OPTION_SEARCH_POST);
+//        System.out.println("TO SEARCH FOR A PROFILE ENTER "+OPTION_SEARCH_PROFILE);
+//        System.out.println("TO VIEW PERSONAL PROFILE ENTER "+OPTION_VIEW_PERSONAL_PROFILE);
 
     }
 
